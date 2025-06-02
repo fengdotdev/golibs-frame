c6425527dc3172b0b767981ecbc2b6d6e826ffcd
fengdotdev <feng@vitruvio.cl>
2025-06-02 07:14:46 -0400

    ....

--- sandbox/draft1/gom8bit/impl_binarer.go
+++ sandbox/draft1/gom8bit/impl_binarer.go
@@ -2,9 +2,6 @@ package gom8bit
 
 var _ Binaer = (*GoM8Bit)(nil) // Ensure GoM8Bit implements Binaer interface
 
-const HeaderGom8bit = "gom8bit"
-
-// <header, version, length, sha 256, data>
 
 // FromBinary implements trait.Binarer.
 func (g *GoM8Bit) FromBinary([]byte) error {
@@ -26,12 +23,45 @@ func (g *GoM8Bit) Data() []byte {
 	if !g.initialized {
 		return nil
 	}
-	data := make([]byte, g.x*g.y)
-	for i := uint(0); i < g.x; i++ {
-		for j := uint(0); j < g.y; j++ {
-			data[i*g.y+j] = g.d[i][j]
+	return Slice2DToData(g.d)
+}
+
+func Slice2DToData(s [][]uint8) []byte {
+	// Convert a 2D slice of uint8 to a 1D byte slice
+	if len(s) == 0 || len(s[0]) == 0 {
+		return nil
+	}
+	data := make([]byte, len(s)*len(s[0]))
+	for i := range s {
+		for j := range s[i] {
+			data[i*len(s[0])+j] = s[i][j]
 		}
 	}
 	return data
 }
+
+func DataToSlice2D(data []byte, x, y uint) [][]uint8 {
+	// Convert a 1D byte slice to a 2D slice of uint8
+	if len(data) == 0 || x == 0 || y == 0 {
+		return nil
+	}
+	matrix := make([][]uint8, x)
+	for i := range matrix {
+		matrix[i] = make([]uint8, y) // Initialize each row with a slice of uint8
+	}
+
+	for i := uint(0); i < x; i++ {
+		for j := uint(0); j < y; j++ {
+			index := i*y + j
+			if index < uint(len(data)) {
+				matrix[i][j] = data[index]
+			} else {
+				matrix[i][j] = 0 // Default value if data is insufficient
+			}
+		}
+	}
+	return matrix
+}
+
+
 
